2ceef26094f62f9d7f15784609498bbf1ed12856
Conan Duval-Manahan <conanduval-manahan@outlook.com>
2023-11-05 20:18:41 +0000

    active storage setup and implementation into nintendo view file

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,5 +2,5 @@ class Game < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
   validates :review_scores, presence: true
-  has_one_attached :image
+  has_one_attached :main_image
 end
