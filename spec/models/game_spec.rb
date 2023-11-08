ed039adf595a4a44ad751ab1686dbe64a65a61ea
Conan Duval-Manahan <conanduval-manahan@outlook.com>
2023-11-08 20:45:41 +0000

    improved tests and model migrations

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -6,5 +6,21 @@ RSpec.describe Game, type: :model do
   it { is_expected.to validate_presence_of(:title) }
   it { is_expected.to validate_presence_of(:description) }
   it { is_expected.to validate_presence_of(:review_scores) }
-  it { is_expected.to have_one_attached(:image) }
+  it { is_expected.to have_one_attached(:main_image) }
+
+  it "is valid with valid attributes" do
+    subject.title = "Anything"
+    subject.description = "Anything"
+  end
+
+
+  it "is not valid without a title" do
+    game = Game.new(title: nil)
+    expect(game).to_not be_valid
+  end
+
+  it "is not valid without a description" do
+    subject.description = nil
+    expect(subject).to_not be_valid
+  end
 end
