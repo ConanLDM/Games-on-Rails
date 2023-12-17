580a8e529184d38f3c88fd3eb085220f3dc89e7c
Conan Duval-Manahan <conanduval-manahan@outlook.com>
2023-12-17 21:24:45 +0000

    Moved test_helpers files into test_helpers/system and fixed other system files
    Now working as previous, with the system tests running in the browser.

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,7 +3,6 @@ require "test_helpers/system/better_rails_system_tests"
 require "test_helpers/system/capybara_setup"
 require "test_helpers/system/cuprite_helpers"
 require "test_helpers/system/cuprite_setup"
-
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :cuprite, using: :chromium, screen_size: [1400, 1400]
 
