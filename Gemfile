c20a6d8698f54274012a7689162255487be03e0c
Conan Duval-Manahan <conanduval-manahan@outlook.com>
2024-01-11 21:22:04 +0000

    Request Specs Updated, Rails Controller Testing Gem added for Extending Request Specs Possibilities
    Added rails-controller-testing gem to assist with the route guidance
    in request test specs to ensure they direct in the right direction.
    
    Testing locally the specs will fail (however they currently point in the
    right direction),this is due to two factors, first
    with the games spec, user authentication is required in the current
    route the spec path takes. In the second scenario with the user request
    spec various partials are opening from Devise and shared partials, in
    particular "devise/sessions/new" is being rendered which is the intended
    outcome and can later be restyled in the spec to achieve this (the same
    applies to the newly added game spec also).

--- Gemfile
+++ Gemfile
@@ -90,6 +90,9 @@ gem "administrate"
 # For Administrate running with ActiveStorage
 gem "administrate-field-active_storage"
 
+# For RSpec request specs
+gem 'rails-controller-testing'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[ mri mingw x64_mingw ]
--- spec/requests/games_spec.rb
+++ spec/requests/games_spec.rb
@@ -1,17 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe "Games", type: :request do
-  describe "GET /games" do
-    it "renders the intended response" do
-      get "/games"
-      expect(response).to have_http_status(:ok)
-    end
-  end
-
-  describe "GET /nintendo" do
-    it "renders the intended response" do
-      get "/nintendo"
-      expect(response).to have_http_status(:ok)
-    end
+RSpec.describe "Games Show page", type: :request do
+  it "renders the Games Show page" do
+    get "/games/show"
+    expect(response).to render_template(:show)
   end
 end
